2702438cc7ef060437498d9dfe02762e89b821ad
arabian9ts <nok14.tm59@gmail.com>
2018-11-21 20:30:22 +0900

    dev: define Disaster model

--- BOKiN/Model/Disaster.swift
+++ BOKiN/Model/Disaster.swift
@@ -6,8 +6,29 @@
 //  Copyright © 2018 RedBottleCoffee. All rights reserved.
 //
 
-import Foundation
+import ObjectMapper
 
-class Disaster {
+class Disaster: Mappable {
+    var id:             Int       =   0
+    var title:          String    =   ""
+    var place:          String    =   ""
+    var scale:          String    =   ""
+    var category:       String    =   ""
+    var datetime:       String    =   ""
+    var description:    String    =   ""
     
+    func mapping(map: Map) {
+        id             <-    map["id"]
+        title          <-    map["title"]
+        place          <-    map["place"]
+        scale          <-    map["scale"]
+        category       <-    map["category"]
+        datetime       <-    map["datetime"]
+        description    <-    map["description"]
+    }
+    
+    required convenience init?(map: Map) {
+        self.init()
+        mapping(map: map)
+    }
 }
