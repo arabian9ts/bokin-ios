46ce085642d37221d1680e610d420a44ec917efb
arabian9ts <nok14.tm59@gmail.com>
2018-11-21 14:31:55 +0900

    add: xibs and contents for disaster table view

---
+++ BOKiN/View/DisastersTableTopCell/DisastersTableTopCell.swift
@@ -0,0 +1,24 @@
+//
+//  DisastersTableTopCell.swift
+//  BOKiN
+//
+//  Created by arabian9ts on 2018/11/21.
+//  Copyright © 2018 RedBottleCoffee. All rights reserved.
+//
+
+import UIKit
+
+class DisastersTableTopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
