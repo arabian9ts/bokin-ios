308bfdacb907d5d2d2b978131f2e06e0e338caeb
arabian9ts <nok14.tm59@gmail.com>
2018-11-21 18:01:19 +0900

    add: news view controller

---
+++ BOKiN/Controller/NewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsViewController.swift
+//  BOKiN
+//
+//  Created by arabian9ts on 2018/11/21.
+//  Copyright © 2018 RedBottleCoffee. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
