0b72caab4593e63dd8193a3f26a14bed4ed7a3f8
arabian9ts <nok14.tm59@gmail.com>
2018-11-21 18:15:08 +0900

    add: news tableview cell

---
+++ BOKiN/View/NewsTableViewCell/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  BOKiN
+//
+//  Created by arabian9ts on 2018/11/21.
+//  Copyright © 2018 RedBottleCoffee. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
