2dd8f23b55a62bd4012ba163ffcfeb56742555b7
arabian9ts <nok14.tm59@gmail.com>
2018-11-21 21:56:48 +0900

    add: Nuke Extension for loading image via http protocol

--- BOKiN/Extension/Nuke+.swift
+++ BOKiN/Extension/Nuke+.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2018 RedBottleCoffee. All rights reserved.
 //
 
-import Foundation
+import Nuke
+import UIKit
+
+func loadImageWithNuke(url: String, imageView: UIImageView) {
+    let options = ImageLoadingOptions(
+        placeholder: UIImage(named: "placeholder"),
+        transition: .fadeIn(duration: 0.33)
+    )
+    Nuke.loadImage(
+        with: URL(string: url) ?? URL(string: "https://haginoryokkou.com/wp-content/uploads/2016/09/noimage.png")!,
+        options: options,
+        into: imageView)
+}
