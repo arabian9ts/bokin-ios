5c64e7725609bb6c7b753bc69d96ef557360754c
arabian9ts <nok14.tm59@gmail.com>
2018-11-21 18:14:34 +0900

    add: news model

---
+++ BOKiN/Model/News.swift
@@ -0,0 +1,33 @@
+//
+//  News.swift
+//  BOKiN
+//
+//  Created by arabian9ts on 2018/11/21.
+//  Copyright © 2018 RedBottleCoffee. All rights reserved.
+//
+
+import ObjectMapper
+
+class News: Mappable {
+    var id:             Int       =   0
+    var title:          String    =   ""
+    var detailUrl:      String    =   ""
+    var summary:        String    =   ""
+    var imageUrl:       String    =   ""
+    var datetime:       String    =   ""
+    var disaster_id:    Int       =   0
+    
+    func mapping(map: Map) {
+        id            <-  map["id"]
+        title         <-  map["title"]
+        detailUrl     <-  map["url"]
+        imageUrl      <-  map["thumbnail_image"]
+        datetime      <-  map["datetime"]
+        disaster_id   <-  map["disaster_id"]
+    }
+    
+    required convenience init?(map: Map) {
+        self.init()
+        mapping(map: map)
+    }
+}
