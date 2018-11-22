4a4a638c7c69fa0871fee090c50c09d370b69397
arabian9ts <nok14.tm59@gmail.com>
2018-11-22 19:57:43 +0900

    add: bokin view controller

---
+++ BOKiN/Controller/BokinViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BokinViewController.swift
+//  BOKiN
+//
+//  Created by arabian9ts on 2018/11/22.
+//  Copyright © 2018 RedBottleCoffee. All rights reserved.
+//
+
+import UIKit
+
+class BokinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
