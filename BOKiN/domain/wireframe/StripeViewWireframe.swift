ed4f682f2e6c6551bfc974f6ef00bf57a0c21345
arabian9ts <nok14.tm59@gmail.com>
2018-11-22 19:54:07 +0900

    dev: update stripe view wireframe

--- BOKiN/domain/wireframe/StripeViewWireframe.swift
+++ BOKiN/domain/wireframe/StripeViewWireframe.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2018 RedBottleCoffee. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+protocol StripeViewWireframe {
+    func transitionToStripeViewPage()
+}
+
+class StripeViewWireframeImpl: StripeViewWireframe {
+    private var transitioner: Transitioner!
+    
+    init(transitioner: Transitioner) {
+        self.transitioner = transitioner
+    }
+    
+    func transitionToStripeViewPage() {
+        let stripeVC = StripeViewBuilder().build()
+        transitioner.transition(to: stripeVC, animated: true, completion: nil)
+    }
+}
