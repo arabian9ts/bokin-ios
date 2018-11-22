8a6ce40c69e7e582a2892dac6dd6ae068e91baea
arabian9ts <nok14.tm59@gmail.com>
2018-11-22 22:15:53 +0900

    add: settlement controller

---
+++ BOKiN/View/SettlementModelView/SettlementModalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettlementModalViewController.swift
+//  BOKiN
+//
+//  Created by arabian9ts on 2018/11/22.
+//  Copyright © 2018 RedBottleCoffee. All rights reserved.
+//
+
+import UIKit
+
+class SettlementModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
