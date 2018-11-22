c85743ea4fc53c76386bf6e0577a54876b7d6335
arabian9ts <nok14.tm59@gmail.com>
2018-11-22 21:40:06 +0900

    fix: delete stripe

--- BOKiN/Generated/storyboards.swift
+++ BOKiN/Generated/storyboards.swift
@@ -3,7 +3,6 @@
 // swiftlint:disable sorted_imports
 import Foundation
 import UIKit
-import BOKiN
 
 // swiftlint:disable superfluous_disable_command
 // swiftlint:disable file_length
@@ -83,11 +82,6 @@ internal enum StoryboardScene {
     internal static let storyboardName = "OriginalNewsSite"
 
     internal static let initialScene = InitialSceneType<BOKiN.OriginalNewsSiteViewController>(storyboard: OriginalNewsSite.self)
-  }
-  internal enum Stripe: StoryboardType {
-    internal static let storyboardName = "Stripe"
-
-    internal static let initialScene = InitialSceneType<BOKiN.StripeViewController>(storyboard: Stripe.self)
   }
 }
 
