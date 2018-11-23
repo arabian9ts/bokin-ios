3fcac6f17d915659cf0db811f9369737bed6051c
arabian9ts <nok14.tm59@gmail.com>
2018-11-24 05:44:14 +0900

    add: navigation logo extension

--- BOKiN/Extension/UIViewController+.swift
+++ BOKiN/Extension/UIViewController+.swift
@@ -1,5 +1,5 @@
 //
-//  UIViewController.swift
+//  UIViewController+.swift
 //  BOKiN
 //
 //  Created by arabian9ts on 2018/11/24.
@@ -10,7 +10,7 @@ import UIKit
 
 extension UIViewController {
     func setLogoImageView() {
-        let titleView: UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        let titleView: UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
         titleView.contentMode = .scaleAspectFit
         titleView.image = UIImage(named: "logo")
         self.navigationItem.titleView = titleView
