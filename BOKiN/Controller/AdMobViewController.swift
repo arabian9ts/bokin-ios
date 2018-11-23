d5d0b2d2676bbf5c003fac29a4de56f05dc96d92
Kansei Kogami <kk7221dev@gmail.com>
2018-11-23 18:20:46 +0900

    delete: debug print

--- BOKiN/Controller/AdMobViewController.swift
+++ BOKiN/Controller/AdMobViewController.swift
@@ -27,34 +27,27 @@ class AdMobViewController: UIViewController, GADRewardBasedVideoAdDelegate {
     }
     
     func rewardBasedVideoAdDidReceive(_ rewardBasedVideoAd:GADRewardBasedVideoAd) {
-//        print("Reward based video ad is received.")
         if rewardBasedAd.isReady {
             rewardBasedAd.present(fromRootViewController: self)
         }
     }
     
     func rewardBasedVideoAdDidOpen(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-//        print("Opened reward based video ad.")
     }
     
     func rewardBasedVideoAdDidStartPlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-//        print("Reward based video ad started playing.")
     }
     
     func rewardBasedVideoAdDidCompletePlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-//        print("Reward based video ad has completed.")
     }
     
     func rewardBasedVideoAdDidClose(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-//        print("Reward based video ad is closed.")
     }
     
     func rewardBasedVideoAdWillLeaveApplication(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
-//        print("Reward based video ad will leave application.")
     }
     
     func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd,
                             didFailToLoadWithError error: Error) {
-//        print("Reward based video ad failed to load.")
     }
 }
