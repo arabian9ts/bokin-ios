bd1c273e2b796362a844c12047d423ce988bd43c
Kansei Kogami <kk7221dev@gmail.com>
2018-11-24 04:56:31 +0900

    add: adMobVideo to AdvertisementSettlementModalView

--- BOKiN/View/AdvertisementSettlementModalView/AdvertisementSettlementModalViewController.swift
+++ BOKiN/View/AdvertisementSettlementModalView/AdvertisementSettlementModalViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class AdvertisementSettlementModalViewController: UIViewController {
     
@@ -14,11 +15,28 @@ class AdvertisementSettlementModalViewController: UIViewController {
     
     private var prefectureList = ["福岡県", "宮崎県", "熊本県", "佐賀県", "長崎県"]
     
+    @IBOutlet weak var adMobButton: UIButton!
+    
+    private var rewardBasedAd: GADRewardBasedVideoAd!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        adMobButton.isEnabled = false
+        
         prefecturePicker.delegate = self
         prefecturePicker.dataSource = self
+        
+        rewardBasedAd = GADRewardBasedVideoAd.sharedInstance()
+        rewardBasedAd.delegate = self
+        rewardBasedAd.load(GADRequest(), withAdUnitID: KeyChainManager.shared.adUnitID)
+    }
+    
+    @IBAction func pushAdMobButton(_ sender: UIButton) {
+        if rewardBasedAd.isReady {
+            adMobButton.isEnabled = false
+            rewardBasedAd.present(fromRootViewController: self)
+        }
     }
 }
 
@@ -40,5 +58,37 @@ extension AdvertisementSettlementModalViewController: UIPickerViewDelegate, UIPi
                     didSelectRow row: Int,
                     inComponent component: Int) {
         
+    }
+}
+
+extension AdvertisementSettlementModalViewController: GADRewardBasedVideoAdDelegate {
+    
+    func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd,
+                            didRewardUserWith reward: GADAdReward) {
+        //        print("Reward received with currency: \(reward.type), amount \(reward.amount).")
+    }
+    
+    func rewardBasedVideoAdDidReceive(_ rewardBasedVideoAd:GADRewardBasedVideoAd) {
+        adMobButton.isEnabled = true
+    }
+    
+    func rewardBasedVideoAdDidOpen(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
+    }
+    
+    func rewardBasedVideoAdDidStartPlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
+    }
+    
+    func rewardBasedVideoAdDidCompletePlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
+    }
+    
+    func rewardBasedVideoAdDidClose(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
+        adMobButton.isEnabled = true
+    }
+    
+    func rewardBasedVideoAdWillLeaveApplication(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
+    }
+    
+    func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd,
+                            didFailToLoadWithError error: Error) {
     }
 }
