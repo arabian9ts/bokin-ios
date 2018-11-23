04fedd84b9d778f9424d7b694753bd606643c882
Kansei Kogami <kk7221dev@gmail.com>
2018-11-23 21:40:04 +0900

    create: ApplePaySettlementModalView, ApplePaySettlementModalViewController

---
+++ BOKiN/View/ApplePaySettlementModalView/ApplePaySettlementModalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ApplePaySettlementModalViewController.swift
+//  BOKiN
+//
+//  Created by 小神寛晴 on 2018/11/23.
+//  Copyright © 2018 RedBottleCoffee. All rights reserved.
+//
+
+import UIKit
+
+class ApplePaySettlementModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
