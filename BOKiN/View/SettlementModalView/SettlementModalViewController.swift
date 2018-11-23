935aae57dee0a5964476ab4e4752221fcc5cbe14
arabian9ts <nok14.tm59@gmail.com>
2018-11-23 15:33:28 +0900

    fix: dismiss modal

--- BOKiN/View/SettlementModalView/SettlementModalViewController.swift
+++ BOKiN/View/SettlementModalView/SettlementModalViewController.swift
@@ -7,10 +7,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SettlementModalViewController: UIViewController {
-
+    
+    private let tapGesture = UITapGestureRecognizer()
+    private let disposeBag = DisposeBag()
+    
+    @IBOutlet var coverView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        coverView.addGestureRecognizer(tapGesture)
+        
+        setupRx()
+    }
+    
+    private func setupRx() {
+        tapGesture.rx.event.bind(onNext: { [self] _ in
+            self.view.alpha = 1.0
+            UIView.animate(withDuration: 0.16, delay: 0.0, options: [.curveEaseOut], animations: {
+                self.view.alpha = 0.0
+            }) { _ in
+                self.view.removeFromSuperview()
+                self.dismiss(animated: true)
+            }
+        })
+        .disposed(by: disposeBag)
     }
 }
